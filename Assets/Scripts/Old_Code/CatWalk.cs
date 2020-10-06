71efd34fdae6738126fcf26f69697e5cb2363a23
Carson <ceg488@nyu.edu>
2020-10-05 22:23:38 -0400

    First repo. set up map set up and movement controls(maybe to fast?).  Set up most prefabs and am working on collision code at the moment.

---
+++ Assets/Scripts/Old_Code/CatWalk.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatWalk : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //if player holds down UP ARROW, then move up
+        if (Input.GetKey(KeyCode.UpArrow)){
+            transform.position += new Vector3(0f, 0.1f, 0f);
+        }
+
+        //if Player holds down RIGHT ARROW, then move right
+        if (Input.GetKey(KeyCode.RightArrow)){
+            transform.position += new Vector3(0.1f, 0f, 0f);
+            //Change Sprite Direction
+            Vector3 characterScale = transform.localScale;
+            characterScale.x = -1;
+            transform.localScale = characterScale;
+        }
+
+        //if Player holds down DOWN ARROW, then move down
+        if (Input.GetKey(KeyCode.DownArrow)){
+            transform.position += new Vector3(0f, -0.1f, 0f);
+        }
+
+        //if Player holds down LEFT ARROW, then move left
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += new Vector3(-0.1f, 0f, 0f); 
+            //Change Sprite Direction
+            Vector3 characterScale = transform.localScale;
+            characterScale.x = 1;
+            transform.localScale = characterScale;
+        }
+    }
+}
---
+++ Assets/Scripts/Old_Code/MouseAim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Purpose: show how to aim turret and fire hitscan wepon based on mouse aiming
+//Usage:put this on player turret obj face east (+x axis)
+public class MouseAim : MonoBehaviour
+{
+    public GameObject explodePreFab; // assign in Inspector
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouse aiming: always face towrds mouse cursor position
+        Vector3 mouseCursorPosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 fromPlayerToMouseCursor =  mouseCursorPosInWorld - transform.position;
+        transform.right = fromPlayerToMouseCursor; //rotate to align my "right" with vec towrds mouse cursor
+        Debug.DrawRay(transform.position, fromPlayerToMouseCursor, Color.yellow);
+
+        //raycast from player towrds mouse cursor, and then instatiate something at impact point.
+        Ray2D myRay = new Ray2D(transform.position, transform.right);
+        float myMaxRayDist = 10f;
+        RaycastHit2D myRayHit = Physics2D.Raycast(myRay.origin, myRay.direction, myMaxRayDist);
+
+        //did we hit something if so instatiate something at impact point
+        if(myRayHit.collider !=null && Input.GetMouseButtonDown(0)){
+            //institiat explostion at impact point
+            Instantiate(explodePreFab, myRayHit.point, Quaternion.Euler(0,0,0));
+            Debug.Log("Raycast is hitting" + myRayHit.collider.name);
+
+             //Destroy (delete raycasthit
+            Destroy(myRayHit.collider.gameObject);
+            
+
+        }
+
+    }
+}
